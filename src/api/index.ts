34e9439acf52a528b330ded54d6896522206b336
WCP <1129342154@qq.com>
2023-01-11 18:29:56 +0800

    feat: 🚀 全局添加请求loading和处理报错提示

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,10 @@
+import { showFullScreenLoading, tryHideFullScreenLoading } from "@/config/serviceLoading";
+import { ResultEnum } from "@/enums/httpEnum";
+import router from "@/router";
 import { GlobalStore } from "@/store";
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { ElMessage } from "element-plus";
+import { checkStatus } from "./helper/checkStatus";
 import { ResultData } from "./interface";
 
 // interface MyAxiosRequestConfig extends AxiosRequestConfig {
@@ -19,6 +24,8 @@ class Request {
 		this.server.interceptors.request.use(
 			(config: AxiosRequestConfig) => {
 				const { token } = GlobalStore();
+				// 如果当前请求不需要显示 loading,在 api 服务中通过指定的第三个参数: { headers: { noLoading: true } }来控制不显示loading
+				config.headers!.noLoading || showFullScreenLoading();
 				return { ...config, headers: { ...config.headers, Authorization: token } };
 			},
 			(error: AxiosError) => {
@@ -26,13 +33,37 @@ class Request {
 				return Promise.reject(error);
 			}
 		);
+
 		this.server.interceptors.response.use(
 			(response: AxiosResponse) => {
 				const { data } = response;
+				const globalStore = GlobalStore();
+				// 请求结束 关闭loading
+				tryHideFullScreenLoading();
+				// 登陆失效（code == 599）
+				if (data.code === ResultEnum.OVERDUE) {
+					ElMessage.error(data.msg);
+					globalStore.setToken("");
+					router.replace("/login");
+					return Promise.reject(data);
+				}
+				// 全局错误信息拦截
+				if (data.code && data.code !== ResultEnum.SUCCESS) {
+					ElMessage.error(data.msg);
+					return Promise.reject(data);
+				}
+				// 成功请求
 				return data;
 			},
 			(error: AxiosError) => {
-				console.log(error);
+				const { response } = error;
+				tryHideFullScreenLoading();
+				// 请求超时单独判断，因为请求超时没有 response
+				if (error.message.indexOf("timeout") !== -1) ElMessage.error("请求超时！请您稍后重试");
+				// 根据响应的错误状态码，做不同的处理
+				if (response) checkStatus(response.status);
+				// 服务器结果都没有返回(可能服务器错误可能客户端断网)，断网处理:可以跳转到断网页面
+				if (!window.navigator.onLine) router.replace("/500");
 				return Promise.reject(error);
 			}
 		);
